4b89423e40c8fb1e24e8c85f04f56995dab8c600
PBardy <58274545+PBardy@users.noreply.github.com>
2023-04-02 18:17:03 +0100

    forgot password

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,15 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, IonicModule, FontAwesomeModule],
   templateUrl: './forgot-password.component.html',
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ForgotPasswordComponent {
-
-}
+export class ForgotPasswordComponent {}
