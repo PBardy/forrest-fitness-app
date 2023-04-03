ee76f1d04a4e260eab3af7da93c0e96464757e8d
PBardy <58274545+PBardy@users.noreply.github.com>
2023-04-03 11:50:22 +0100

    Add links to sign in sign up screen

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, IonicModule, FontAwesomeModule],
+  imports: [CommonModule, IonicModule, RouterModule, FontAwesomeModule],
   templateUrl: './landing.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    fontFamily: {
-      sans: ["FigTree", "Arial", "sans-serif"],
-    },
     extend: {
+      fontFamily: {
+        sans: ["FigTree", "Arial", "sans-serif"],
+      },
       colors: {
         primary: "var(--ion-color-primary)",
         primary_tint: "var(--ion-color-primary-tint)",
