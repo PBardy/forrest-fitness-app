63b8741b72624c930e4ad1afb4ad09fb7f261eb2
PBardy <58274545+PBardy@users.noreply.github.com>
2023-04-03 19:13:56 +0100

    edit activity

--- src/app/features/edit-activity/edit-activity.component.ts
+++ src/app/features/edit-activity/edit-activity.component.ts
@@ -1,15 +1,100 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Store } from '@ngrx/store';
+import { LetModule } from '@ngrx/component';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Activity, Workout } from '@types';
+import { ActivityActions } from '@app/store/activity/activity.actions';
+import { initialWorkouts } from '@app/store/workout/workout.reducer';
+import { ReplaySubject, filter, map, switchMap, takeUntil, tap } from 'rxjs';
+import { selectActivitiesById } from '@app/store/activity/activity.selectors';
 
 @Component({
   selector: 'app-edit-activity',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './edit-activity.component.html',
-  styles: [
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule,
+    FontAwesomeModule,
+    LetModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: './edit-activity.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditActivityComponent {
+export class EditActivityComponent implements OnInit, OnDestroy {
+  public workouts$ = initialWorkouts;
+  public destroy$ = new ReplaySubject<boolean>(1);
 
+  public activity$ = this.route.paramMap.pipe(
+    map((x) => x.get('id') as string),
+    switchMap((x) => this.store.select(selectActivitiesById(x)))
+  );
+
+  public form = this.fb.nonNullable.group({
+    title: this.fb.nonNullable.control<string>('', [Validators.required]),
+    workout: this.fb.nonNullable.control<Workout | null>(null, [
+      Validators.required,
+    ]),
+    start: this.fb.nonNullable.control<string>('', [Validators.required]),
+    intensity: this.fb.nonNullable.control<number | undefined>(undefined),
+    duration: this.fb.nonNullable.control<number | undefined>(undefined),
+    distance: this.fb.nonNullable.control<number | undefined>(undefined),
+    energy: this.fb.nonNullable.control<number | undefined>(undefined),
+    steps: this.fb.nonNullable.control<number | undefined>(undefined),
+    notes: this.fb.nonNullable.control<string>(''),
+  });
+
+  public constructor(
+    private readonly fb: FormBuilder,
+    private readonly store: Store,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
+
+  public ngOnInit(): void {
+    this.activity$
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(Boolean),
+        tap((x) => this.form.patchValue(x))
+      )
+      .subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  public onCancel() {
+    this.router.navigate(['app', 'activities', 'all']);
+  }
+
+  public onSave(id: string) {
+    const value = this.form.getRawValue() as NonNullable<Activity>;
+    const payload = { id, ...value };
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.store.dispatch(ActivityActions.updateone({ payload }));
+  }
 }
--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -9,8 +9,8 @@ export class ToastService {
   public constructor(private readonly toastController: ToastController) {}
 
   public present(opts?: ToastOptions) {
-    return from(this.toastController.create({ ...opts, duration: 2000 })).pipe(
-      switchMap((t) => of(t.present()))
-    );
+    return from(
+      this.toastController.create({ ...opts, position: 'top', duration: 2000 })
+    ).pipe(switchMap((t) => of(t.present())));
   }
 }
